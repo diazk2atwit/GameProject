0a3121307a859b40e3455ddce152f9e90324dc3e
diazk2atwit <54991291+diazk2atwit@users.noreply.github.com>
2020-04-08 17:04:35 -0400

    Add files via upload

---
+++ GameProject/src/module-info.java
@@ -0,0 +1,10 @@
+module javafx {
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swt;
+	requires javafx.web;
+	exports racing_game;
+}
